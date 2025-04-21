3d9c692b1d8b608fdda6f98247bf64c754d9a04e
Joseph Lundy <joseph.lundy456@gmail.com>
2025-04-21 12:54:45 -0400

    adds code to show webcam and command to save webcam image

--- supervisor.py
+++ supervisor.py
@@ -1,21 +1,42 @@
 # Importing Libraries 
 import serial 
-import time 
 import msvcrt
+import cv2
+
+cam_port = 0
+cam = cv2.VideoCapture(cam_port)
+
 
 arduino = serial.Serial(port='COM3', baudrate=9600, timeout=.1) 
-def write_read(x): 
-    arduino.write(bytes(x, 'utf-8')) 
-    time.sleep(0.05) 
-    return data 
+
+capture_count = 0
 
 while True: 
+
+    result, image = cam.read()
+    if result:
+        cv2.imshow("webcam", image)
+
     data = arduino.readline().decode()
     if(data != ''):
         print(data) # printing the value 
 
     if msvcrt.kbhit():
-        # command = msvcrt.getch();
-        command = input("Enter a number: ") # Taking input from user 
+        command = input("Enter a command: ") # Taking input from user
         print("You input: ", command)
-        arduino.write(bytes(command, 'utf-8'))
+        if(command == 'q'):
+            print("exiting")
+            break
+        elif (command == 'c'):
+            # capture
+            fileName = "webcam_" + str(capture_count) + ".png"
+            print("saving image")
+            cv2.imwrite(fileName, image)
+            capture_count += 1
+        else:
+            arduino.write(bytes(command, 'utf-8'))
+
+    cv2.waitKey(1)
+
+cam.release()
+cv2.destroyAllWindows()
