be1d82121e9c55022a984dbfc483ba28d554e0de
Joseph Lundy <joseph.lundy456@gmail.com>
2025-04-24 18:48:58 -0400

    wip adding image processing scripts

---
+++ image_processing/evalute_calibration.py
@@ -0,0 +1,86 @@
+import numpy as np
+import cv2 as cv
+import glob
+import argparse
+import os
+from pathlib import Path
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--folder", help = "path to the image file")
+args = vars(ap.parse_args())
+folder = args["folder"]
+
+print(folder)
+# if folder is None:
+#     folder = Path("C:\Users\littl\OneDrive - University of Toronto\Winter 2025\CSC2530\Project\code")
+
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+ 
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+ 
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+search_string = folder + "/*.png"
+images = glob.glob(search_string)
+ 
+for fname in images:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+ 
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+ 
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+ 
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+ 
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (7,6), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(500)
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+# Evaluation
+img = cv.imread(images[0])
+h,  w = img.shape[:2]
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+
+# undistort
+mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
+dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
+ 
+# crop the image
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv.imwrite('calibresult.png', dst)
+
+print("Camera matrix")
+print(mtx)
+print("Distortion")
+print(dist)
+
+outfile = "calibration.npz"
+np.savez(outfile, mtx=mtx, dist=dist)
+
+mean_error = 0
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+    mean_error += error
+ 
+print( "total error: {}".format(mean_error/len(objpoints)) )
+
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+# C:\Users\littl\OneDrive - University of Toronto\Winter 2025\CSC2530\Project\datasets\calibration\camera_module\1\calibrationdata
